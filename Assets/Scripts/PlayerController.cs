46753dc4e1d375de5eb2104edbe20c2575e0a8ab
rupamkairi <rupamkairi@hotmail.com>
2018-07-15 12:23:19 +0530

    optimized UI for phones

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,12 +20,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
 
 
+    // Make call to Setup Game
     void Awake()
     {
         levelScript = GetComponent<LevelGenerator>();
         initGame();
     }
 
+    // Setup Game
     public void initGame()
     {
         levelScript.SetupScene();
@@ -33,29 +35,39 @@ public class PlayerController : MonoBehaviour
 		Debug.Log(CoinCount);
     }
 
+    // Starts Game Elements
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
+        // Set Points at the start.
+        /// <Save_From_Last_Scene>
         Money = 5;
         SetMoneyText();
 
+        // Set Ending Text component and setting it to null
         EndText.text = null;
 		levelText.text = "LEVEL " + Level.ToString();
     }
 
     void Update()
     {
+        // If Player get caught
         if (isCaught)
         {
+            // Slower the time and Restart, show End Text, go to another Scene
             Time.timeScale = 0.2f;
+            EndText.text = "ARRESTED! 5 MONEY PENALTY";
+            Money -= 5;
             StartCoroutine(Restart());
         }
 
+        // If Player collected all Coins
 		if (CoinCount == 0)
 		{
+            // Slower the time, Congratulate, go to the another level or Scene
 			Debug.Log("Win");
-			Time.timeScale = 0.4f;
+			Time.timeScale = 0.2f;
 			EndText.text = "GREAT ! LEVEL CLEARED";
 			Level++;
 			StartCoroutine(Restart());
@@ -67,17 +79,21 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        // Setting movement for Keyboard
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.velocity = (movement * speed);
 
+        // Setting movement for Touch Input devices
         joystickControl();
     }
 
     void OnTriggerEnter(Collider other)
     {
+        // colletct Coins when collided
         if (other.gameObject.tag == "Coin")
         {
+            // remove Coin after collision, Increase Point
             Destroy(other.gameObject);
             Money += 2;
             CoinCount--;
@@ -87,12 +103,12 @@ public class PlayerController : MonoBehaviour
 
         if (other.gameObject.tag == "Enemy")
         {
+            // If collided to AI, Player get caught
             isCaught = true;
-            EndText.text = "ARRESTED ! 5 MONEY PENALTY";
-            Money -= 5;
         }
     }
 
+    // Set Money amount to Money Text
     void SetMoneyText()
     {
         PointText.text = "MONEY " + Money.ToString();
@@ -105,6 +121,8 @@ public class PlayerController : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
+    // Logic for Joystic type touch control
+    /// <Get_Asset> from <https://assetstore.unity.com/packages/tools/input-management/joystick-pack-107631>
     void joystickControl()
     {
         Vector3 moveVector = (transform.right * joystick.Horizontal + transform.forward * joystick.Vertical).normalized;
